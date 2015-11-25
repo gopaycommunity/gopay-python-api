7de492ee140438c00a63f4db10d2653cfceb0266
Zdeněk Drahoš <drahoszdenek@gmail.com>
2015-11-25 13:58:08 +0100

    API - define remaining api methods

--- gopay/payments.py
+++ gopay/payments.py
@@ -3,26 +3,49 @@ from http import Request
 
 class Payments:
     def __init__(self, browser):
-        self.browser = browser
+        self.gopay = GoPay(browser)
 
     def create_payment(self, payment):
-        request = Request()
-        request.url = 'https://gw.sandbox.gopay.com/api/payments/payment'
-        request.method = 'post'
-        request.headers = {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
-        request.body = payment
-        return self.browser.browse(request)
+        return self.gopay.json_call('', payment)
 
     def get_status(self, id):
+        return self.gopay.form_call('/' + str(id), None)
+
+    def refund_payment(self, id, amount):
+        return self.gopay.form_call('/' + str(id) + '/refund', {'amount': amount})
+
+    def create_recurrence(self, id, payment):
+        return self.gopay.json_call('/' + str(id) + '/create-recurrence', payment)
+
+    def void_recurrence(self, id):
+        return self.gopay.form_call('/' + str(id) + '/void-recurrence', {})
+
+    def capture_authorization(self, id):
+        return self.gopay.form_call('/' + str(id) + '/capture', {})
+
+    def void_authorization(self, id):
+        return self.gopay.form_call('/' + str(id) + '/void-authorization', {})
+
+class GoPay:
+    def __init__(self, browser):
+        self.browser = browser
+
+    def json_call(self, url, data):
+        return self.call(url, 'application/json', data)
+
+    def form_call(self, url, data):
+        return self.call(url, 'application/x-www-form-urlencoded', data)
+
+    def call(self, url, content_type, data):
         request = Request()
-        request.url = 'https://gw.sandbox.gopay.com/api/payments/payment/' + str(id)
-        request.method = 'get'
+        request.url = 'https://gw.sandbox.gopay.com/api/payments/payment' + url
         request.headers = {
             'Accept': 'application/json',
-            'Content-Type': 'application/x-www-form-urlencoded'
+            'Content-Type':  content_type
         }
+        if (data is None):
+            request.method = 'get'
+        else:
+            request.method = 'post'
+            request.body = data
         return self.browser.browse(request)
-
--- tests/payments_test.py
+++ tests/payments_test.py
@@ -14,17 +14,22 @@ class PaymentsTest(unittest.TestCase):
         assert_that(response, is_(self.browser.response))
 
     endpoints = lambda: (
-        (lambda p: p.create_payment({'payment': ''}), 'https://gw.sandbox.gopay.com/api/payments/payment', 'post', is_not),
-        (lambda p: p.get_status(3), 'https://gw.sandbox.gopay.com/api/payments/payment/3', 'get', is_),
+        (lambda p: p.create_payment({'payment': ''}), '', 'post', is_not),
+        (lambda p: p.get_status(3), '/3', 'get', is_),
+        (lambda p: p.refund_payment(3, 100), '/3/refund', 'post', is_not),
+        (lambda p: p.create_recurrence(3, {'payment': ''}), '/3/create-recurrence', 'post', is_not),
+        (lambda p: p.void_recurrence(3), '/3/void-recurrence', 'post', is_),
+        (lambda p: p.capture_authorization(3), '/3/capture', 'post', is_),
+        (lambda p: p.void_authorization(3), '/3/void-authorization', 'post', is_),
     )
 
     @data_provider(endpoints)
-    def test_should_build_request(self, call_api, url, method, has_body):
+    def test_should_build_request(self, call_api, url, method, has_empty_body):
         call_api(self.payments)
-        assert_that(self.browser.request.url, is_(url))
+        assert_that(self.browser.request.url, is_('https://gw.sandbox.gopay.com/api/payments/payment' + url))
         assert_that(self.browser.request.method, is_(method))
         assert_that(self.browser.request.headers, is_not({}))
-        assert_that(self.browser.request.body, has_body({}))
+        assert_that(self.browser.request.body, has_empty_body({}))
 
 
 class BrowserSpy:
