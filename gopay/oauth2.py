e275e31d7c5d9264480e8dc80afa6100a66f08b3
Zdeněk Drahoš <drahoszdenek@gmail.com>
2015-11-25 12:23:30 +0100

    API - build oauth request

--- gopay/oauth2.py
+++ gopay/oauth2.py
@@ -1,9 +1,25 @@
 
+from http import Request
+
 class OAuth2:
-    def __init__(self, config):
+    def __init__(self, config, browser):
         self.config = config
+        self.browser = browser
 
     def authorize(self):
+        request = Request()
+        request.url = 'https://gw.sandbox.gopay.com/api/oauth2/token'
+        request.method = 'post'
+        request.headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': self.config['clientId'] + ':' + self.config['clientSecret']
+        }
+        request.body = {
+            'grant_type': 'client_credentials',
+            'scope': self.config['scope']
+        }
+        response = self.browser.browse(request)
         return AccessToken()
 
 class AccessToken:
--- tests/oauth2_test.py
+++ tests/oauth2_test.py
@@ -4,11 +4,38 @@ from gopay.oauth2 import OAuth2, AccessToken
 
 class OAuth2Test(unittest.TestCase):
 
-    def test_should_return_access_token(self):
-        oauth = OAuth2({
-            'clientId': 'user',
-            'clientSecret': 'pass',
-            'scope': 'irrelevant scope'
-        })
+    def test_should_call_api_and_return_access_token(self):
+        browser = BrowserSpy()
+        oauth = OAuth2(
+            {
+                'clientId': 'userId',
+                'clientSecret': 'pass',
+                'scope': 'irrelevant scope'
+            },
+            browser
+        )
         token = oauth.authorize()
         assert_that(token, is_(instance_of(AccessToken)))
+        assert_that(browser.request.url, is_('https://gw.sandbox.gopay.com/api/oauth2/token'))
+        assert_that(browser.request.method, is_('post'))
+        assert_that(browser.request.headers, is_({
+            'Accept': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'userId:pass'
+        }))
+        assert_that(browser.request.body, is_({
+            'grant_type': 'client_credentials',
+            'scope': 'irrelevant scope'
+        }))
+
+
+class BrowserSpy:
+    def __init__(self):
+        self.request = None
+
+    def browse(self, request):
+        self.request = request
+        return MockResponse()
+
+class MockResponse:
+    pass
