55b6007f4aa9614bb4b057ee7c02fa21421bae1c
frantisek.sichinger <frantisek.sichinger@gopay.cz>
2021-12-07 11:30:02 +0100

    1.2.8

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='gopay',
-    version='1.2.7',
+    version='1.2.8',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/gopaycommunity/gopay-python-sdk',
