b6ce7e1de7039ecf6fc1d8662e27b3ea3cc24c84
Štefan Földesi <stefan.foldesi@outlook.com>
2022-03-22 16:24:04 +0100

    Fix more URL problems

--- gopay/api.py
+++ gopay/api.py
@@ -14,12 +14,19 @@ class GoPay:
         self.config = config
 
     def url(self, path: str):
-        if 'gatewayUrl' in self.config:
-            host = self.config['gatewayUrl']
-            if not host.endswith('/api/'):
-                host += "api/" if host.endswith('/') else "/api/"
-                return host + path
-        host = 'https://gate.gopay.cz/api/' if self.config['isProductionMode'] else 'https://gw.sandbox.gopay.com/api/'
+        if "gatewayUrl" in self.config:
+            host = self.config["gatewayUrl"]
+            if host.endswith("/"):
+                host = host[:-1]
+            if not host.endswith("api"):
+                host += "api"
+            host += "/"
+            return host + path
+        host = (
+            "https://gate.gopay.cz/api/"
+            if self.config["isProductionMode"]
+            else "https://gw.sandbox.gopay.com/api/"
+        )
         return host + path
 
     def call(self, url: str, content_type: str, authorization: str, data: Dict) -> Response:
@@ -32,7 +39,7 @@ class GoPay:
             'Authorization': authorization
         }
         if content_type:
-            request.headers["Content-Type"] = content_type 
+            request.headers["Content-Type"] = content_type
 
         if data is None:
             request.method = 'get'
