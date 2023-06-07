b05d0ca475adbc50dde581011c6bef0ab1c64159
Štefan Földesi <stefan.foldesi@gopay.cz>
2023-06-07 13:06:22 +0200

    Migrate preauthorization unit tests to pytest

---
+++ tests/test_preauthorization.py
@@ -0,0 +1,38 @@
+from gopay import Payments
+import logging
+
+
+class TestPreauthorization:
+    def test_create_preauthorized_payment(self, payments: Payments, base_payment: dict):
+        base_payment.update({"preauthorization": True})
+
+        response = payments.create_payment(base_payment)
+        response_body = response.json
+        logging.info(f"API Response: {response_body}")
+
+        assert "errors" not in response_body
+        assert "id" in response_body
+        assert response_body["state"] == "CREATED"
+        assert response_body["preauthorization"]["state"] == "REQUESTED"
+
+    def test_capture_authorization(self, payments: Payments):
+        payment_id = 3049519447
+
+        response = payments.capture_authorization(payment_id)
+        response_body = response.json
+        logging.info(f"API Response: {response_body}")
+
+        assert "errors" in response_body
+        error_dict = response_body["errors"][0]
+        assert error_dict["error_name"] == "PAYMENT_CAPTURE_DONE"
+
+    def test_void_authorization(self, payments: Payments):
+        payment_id = 3049519343
+
+        response = payments.capture_authorization(payment_id)
+        response_body = response.json
+        logging.info(f"API Response: {response_body}")
+
+        assert "errors" in response_body
+        error_dict = response_body["errors"][0]
+        assert error_dict["error_name"] == "PAYMENT_CAPTURE_FAILED"
