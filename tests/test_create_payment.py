a183a7ddc70bcc0bf9ac9177a543d086f71b98cf
Štefan Földesi <stefan.foldesi@gopay.cz>
2023-06-07 11:59:13 +0200

    Base payments test migrated to pytest

--- tests/test_create_payment.py
+++
@@ -1,8 +0,0 @@
-from gopay import Payments
-
-
-class TestCreatePayment:
-    def test_create_payment(self, payments: Payments, base_payment: dict):
-        response = payments.create_payment(base_payment)
-        response_body = response.json
-        assert "error_code" not in response_body
---
+++ tests/test_payments.py
@@ -0,0 +1,28 @@
+from gopay import Payments
+import logging
+
+
+class TestPayments:
+    def test_create_payment(self, payments: Payments, base_payment: dict):
+        response = payments.create_payment(base_payment)
+        response_body = response.json
+        logging.info(f"API Response: {response_body}")
+        assert "error_code" not in response_body
+
+    def test_refund_payment(self, payments: Payments):
+        payment_id = 3049525986
+
+        response = payments.refund_payment(payment_id, 1900)
+        response_body = response.json
+        logging.info(f"API Response: {response_body}")
+        error_dict = response_body["errors"][0]
+        assert error_dict["error_name"] == "PAYMENT_REFUND_NOT_SUPPORTED"
+
+    def test_payment_status(self, payments: Payments):
+        payment_id = 3049525986
+        response = payments.get_status(payment_id)
+        response_body = response.json
+        logging.info(f"API Response: {response_body}")
+        assert "error_code" not in response_body
+        assert response_body["id"] == payment_id
+        assert response_body["state"] == "REFUNDED"
