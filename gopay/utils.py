5a3297edc27927d58b1d0b7764b21ec1da692125
pmaryska <patrik.maryska@gopay.cz>
2024-10-04 10:22:41 +0200

    Fixed missing pyproject.toml file

--- gopay/utils.py
+++ gopay/utils.py
@@ -1,11 +1,2 @@
-from pathlib import Path
-
-import tomli
-
-def get_project_version():
-    pyproject_path = Path(__file__).resolve().parent.parent / 'pyproject.toml'
-    with open(pyproject_path, 'rb') as file:
-        pyproject_data = tomli.load(file)
-    return pyproject_data['tool']['poetry']['version']
-
-DEFAULT_USER_AGENT = "GoPay Python " + get_project_version()
+VERSION = "2.2.3"
+DEFAULT_USER_AGENT = "GoPay Python " + VERSION
